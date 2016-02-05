b623e216ccb134887c2cdc7f57803a19bf5e0983
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-02-05 11:09:02 +1100

    Update Clipboard.cs

--- src/Clipboard.Droid/Clipboard.cs
+++ src/Clipboard.Droid/Clipboard.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 
+// var applicationContext = Application.Context;
+// ClipboardManager clipboard = (ClipboardManager)applicationContext.GetSystemService(Context.ClipboardService);
+// ClipData clip = ClipData.NewPlainText("someIdentifier",clipboardText);
+// clipboard.PrimaryClip = clip;
+
 namespace Clipboard.Droid
 {
     public class Clipboard : IClipboard
